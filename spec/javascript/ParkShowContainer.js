95a2c7b393bea8b8f88013d316752656d4c647b1
ritazhousmile <ritazhousmile@gmail.com>
2019-07-18 13:55:08 -0400

    finish enzyme test

--- spec/javascript/ParkShowContainer.js
+++ spec/javascript/ParkShowContainer.js
@@ -17,47 +17,36 @@ describe('ParkShowContainer', () => {
       zip:"06661"
     }
 
-    fetchMock.get('/api/v1/parks/1', {
-      status: 200,
-      body: park
-    })
 
-    wrapper = mount(<ParkShowContainer match={routerParams}/>);
+
+    wrapper = shallow(<ParkShowContainer match={routerParams}/>);
   })
 
-  afterEach(fetchMock.restore)
+
 
   it('should have the specified initial state', () => {
     expect(wrapper.state()).toEqual({park: {}})
   });
 
-  it('renders a park fetched from api call', (done) => {
-    setTimeout(() => {
-      expect(wrapper.find('h3').text()).toEqual(park.name)
-        done()
-    }, 0)
-  })
 
-  it('should render a ParkShow Component', (done) => {
-    setTimeout(() => {
-      expect(wrapper.find(ParkShow)).toBePresent()
-        done()
-    }, 0)
+  it('should render a ParkShow Component', () => {
+    wrapper.setState({park: park})
+
+      expect(wrapper.find("ParkShow")).toBePresent()
   })
 
 
-  it('should render the ParkShow Component with specific props when specfic park is selected', (done) => {
-    setTimeout(() => {
-      expect(wrapper.find(ParkShow).props()).toEqual({
-        id:1,
-        name:"Mattapan Park",
-        description:"This place is dangerous",
-        address:"19 State Road",
-        city:"Mattapan",
-        state:"MA",
-        zip:"06661"
-      })
-        done()
-    }, 0)
+  it('should render the ParkShow Component with specific props when specfic park is selected', () => {
+    wrapper.setState({park: park})
+
+    expect(wrapper.find("ParkShow").props()).toEqual({
+      id:1,
+      name:"Mattapan Park",
+      description:"This place is dangerous",
+      address:"19 State Road",
+      city:"Mattapan",
+      state:"MA",
+      zip:"06661"
+    })
   })
 })
