373748d7da3050518bd1b099dd1a6037cab39b44
Chris Lay <chrisklay93@gmail.com>
2019-07-19 14:42:37 -0400

    Removed binding.pry

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,16 +14,11 @@ class ReviewsController < ApplicationController
     @park = Park.find(params[:park_id])
     @review.user_id = current_user.id if current_user
     @review.park = @park
-    binding.pry
     if @review.save
       flash[:success] = "Review add successfully"
       redirect_to @park
     else
-<<<<<<< HEAD
       flash[:error] = @review.errors.full_messages.join(", ")
-=======
-      flash[:notice] = @review.errors.full_messages.join(", ")
->>>>>>> 1d17eacef26c9ed192c42979389ffb9419551296
       render :new
     end
   end
