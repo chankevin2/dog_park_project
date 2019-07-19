c09c713f49a9a758ceaf775f1b58579ef6c155c9
detholm <didrik.etholm@gmail.com>
2019-07-19 15:46:32 -0400

    fixed form def create line 14

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @park = Park.find(params[:park_id])
+    @review.user_id = current_user.id if current_user
     @review.park = @park
       if @review.save
         flash[:success] = "Review added successfully"
