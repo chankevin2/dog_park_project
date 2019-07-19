ceb2a5cb4c79124f4cfe99e57c2bf6244839562e
detholm <didrik.etholm@gmail.com>
2019-07-19 11:13:05 -0400

    Updated reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,16 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @park = Park.find(params[:park_id])
+    @review.user_id = current_user.id if current_user
     @review.park = @park
     if @review.save
       flash[:notice] = "Review add successfully"
-      redirect_to @park 
+      redirect_to @park
     else
-      flash[:notice] = @review.error.full_messages.join(", ")
+      flash[:notice] = @review.errors.full_messages.join(", ")
       render :new
   end
+end
 
   private
 
