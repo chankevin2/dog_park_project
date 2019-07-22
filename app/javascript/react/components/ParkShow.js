5dd3d8b70ca2ce3f55bd0ba83418fb8c3e15d0d6
Chris Lay <chrisklay93@gmail.com>
2019-07-22 13:57:31 -0400

    Changed all paragraphs to have <p> tag

--- app/javascript/react/components/ParkShow.js
+++ app/javascript/react/components/ParkShow.js
@@ -6,10 +6,10 @@ const ParkShow = props => {
     <div>
       <h3>{props.name}</h3>
       <p>Description: {props.description}</p>
-      <p1>Address: {props.address} </p1>
-      <p2>{props.city} </p2>
-      <p3>{props.state} </p3>
-      <p4>{props.zip} </p4>
+      <p>Address: {props.address} </p>
+      <p>{props.city} </p>
+      <p>{props.state} </p>
+      <p>{props.zip} </p>
     </div>
   )
 }
