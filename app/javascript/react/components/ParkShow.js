8388940dacfb1e295a1e4f4919f882abef872293
Chris Lay <chrisklay93@gmail.com>
2019-07-22 14:06:38 -0400

    Edited <p> tags

--- app/javascript/react/components/ParkShow.js
+++ app/javascript/react/components/ParkShow.js
@@ -6,7 +6,11 @@ const ParkShow = props => {
     <div className="park">
       <h3>{props.name}</h3>
       <p>Description: {props.description}</p>
-      <p>Address: {props.address}, {props.city}, {props.state}  {props.zip} </p>
+      <p>Address: {props.address}</p>
+      <p>{props.city},</p>
+      <p>{props.state}</p>
+      <p>{props.zip}</p>
+      </p>
       <br></br>
       <a href={`/parks/${props.id}/reviews/new`}>Add New Review</a>
     </div>
