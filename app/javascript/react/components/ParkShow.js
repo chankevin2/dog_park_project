4d97c9180aba46e57b6ea91ea9fa64de4c4b9084
Chris Lay <chrisklay93@gmail.com>
2019-07-22 14:34:25 -0400

    Added ids to <p> tags

--- app/javascript/react/components/ParkShow.js
+++ app/javascript/react/components/ParkShow.js
@@ -5,8 +5,8 @@ const ParkShow = props => {
   return(
     <div className="park">
       <h3>{props.name}</h3>
-      <p>Description: {props.description}</p>
-      <p>Address: {props.address} {props.city}, {props.state} {props.zip}</p>
+      <p id="descr">Description: {props.description}</p>
+      <p id="address">Address: {props.address} {props.city}, {props.state} {props.zip}</p>
       <br></br>
       <a href={`/parks/${props.id}/reviews/new`}>Add New Review</a>
     </div>
