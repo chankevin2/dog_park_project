67516c5ce50765f08f8aca892ad7add0756ed04f
Chris Lay <chrisklay93@gmail.com>
2019-07-22 14:08:31 -0400

    Changed <p> tags

--- app/javascript/react/components/ParkShow.js
+++ app/javascript/react/components/ParkShow.js
@@ -5,11 +5,11 @@ const ParkShow = props => {
   return(
     <div>
       <h3>{props.name}</h3>
-      <p>Description: {props.description}</p>
-      <p>Address: {props.address} </p>
-      <p>{props.city} </p>
-      <p>{props.state} </p>
-      <p>{props.zip} </p>
+      Description: <p>{props.description}</p>
+      Address: <p>{props.address}</p>
+      <p>{props.city}</p>
+      <p>{props.state}</p>
+      <p>{props.zip}</p>
     </div>
   )
 }
