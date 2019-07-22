7736045b014ae860824ff209dd0065e2df2a83b1
Chris Lay <chrisklay93@gmail.com>
2019-07-22 11:27:34 -0400

    Updated admin tests

--- spec/features/05_user_can_add_parks_spec.rb
+++ spec/features/05_user_can_add_parks_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 feature 'only admins can add parks' do
   scenario 'non-admin user tries to add new park' do
     user = FactoryBot.create(:user, role: "member")
-    
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
     visit new_park_path
 
     expect(page).to have_content "You do not have access to this page"
@@ -11,6 +16,11 @@ feature 'only admins can add parks' do
 
   scenario 'admin adds new park successfully' do
     user = FactoryBot.create(:user, role: "admin")
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
 
     visit new_park_path
 
@@ -24,6 +34,7 @@ feature 'only admins can add parks' do
     fill_in 'Zip', with: "19147"
     fill_in 'Description', with: "Run by the one and only Florida Man"
 
+    expect(page).to have_button("Add Dog Park", disabled: false)
     click_button "Add Dog Park"
 
     expect(page).to have_content "Dog Park saved successfully"
@@ -34,9 +45,16 @@ feature 'only admins can add parks' do
   scenario "admin does not provide proper information for a park" do
    user = FactoryBot.create(:user, role: "admin")
 
+   visit new_user_session_path
+   fill_in 'Email', with: user.email
+   fill_in 'Password', with: user.password
+   click_button 'Log in'
+
    visit new_park_path
 
+   expect(page).to have_button("Add Dog Park", disabled: false)
    click_button "Add Dog Park"
+
    expect(page).to have_content "Name can't be blank"
    expect(page).to have_content "Address can't be blank"
    expect(page).to have_content "City can't be blank"
