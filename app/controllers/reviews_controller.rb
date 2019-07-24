91d4b865604b8c195b8150424770fac692428206
Chris Lay <chrisklay93@gmail.com>
2019-07-24 10:48:31 -0400

    Added edit review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,40 @@
 class ReviewsController < ApplicationController
-  before_actiob :find_review, only: [:new, :show. :edit, :update, :destroy]
+  before_action :find_review, only: [:new, :edit, :update, :destroy]
   def index
     @reviews = Review.all.order("created_at DESC")
   end
-  def show
-  end
+
   def new
     @review = Review.new
+    @park = Park.find(params[:park_id])
   end
+
   def edit
+    @park = Park.find(params[:park_id])
+    @review = Review.find(params[:id])
   end
+
   def update
+    @park = Park.find(params[:park_id])
+    @review = Review.find(params[:id])
     if @review.update(review_params)
-      redirect_to @park
-    else 
-      render 'edit'
+      flash[:success] = "Review edited successfully"
+      redirect_to park_path(@park)
+    else
+      flash.now[:error] = @review.errors.full_messages.join("<br/>").html_safe
+      render :edit
+    end
   end
+
   def destroy
+    @park = Park.find(params[:park_id])
+    @review = Review.find(params[:id])
+
+    @review.destroy
+    flash[:success] = "Review deleted successfully"
+    redirect_to park_path(@park)
   end
+
   def create
     @review = Review.new(review_params)
     @park = Park.find(params[:park_id])
