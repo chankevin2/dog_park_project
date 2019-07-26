8fc5a36ea0af8b0bb05fa3e08e1702e1844a320c
Chris Lay <chrisklay93@gmail.com>
2019-07-26 10:57:30 -0400

    Touched the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_07_23_141632) do
-=======
 ActiveRecord::Schema.define(version: 2019_07_25_140946) do
->>>>>>> origin
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
